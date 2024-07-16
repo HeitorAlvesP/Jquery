38a4ed24dfb02fa1b8377deefa82e15e6844fa2e
Heitor Alves Pinto <heitorpinto.oficial@gmail.com>
2024-07-15 23:01:59 -0300

    add

---
+++ script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    //console.log($(".lista-teste li")[2]) || Primeiro --first || ultimo --last || [] --selecionado
+
+    /*$(".lista-teste").each((index, item) => {
+        $(item).text($(item).text()+"ok")
+    }) */
+
+    $(".lista-teste .ativo_1").css({
+        "background-color": "green"
+    })
+});
