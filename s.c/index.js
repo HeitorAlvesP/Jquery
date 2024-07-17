9c4a416ae4415a2becf41b70aa7c7251e9edad82
Heitor Alves Pinto <134733931+HeitorAlvesP@users.noreply.github.com>
2024-07-17 13:17:33 -0300

    Add files via upload

--- s.c/index.js
+++ s.c/index.js
@@ -4,24 +4,44 @@ $(document).ready(function(){
     setInterval(function() {
         $('h1').fadeOut(2000).fadeIn(5000);
     }, 1000);
-    $('h1').append('De pessoas');
+
+    $('h2').append(' de cores');
+    $('#btn_mostrar_apagar').text('Apagar')
 
     //Mostrar e apagar lista de nomes
     $('#btn_mostrar_apagar').on('click', function(){
         if ($('li').is(':visible')) {
-            $('li').fadeOut(1);
+            $('#btn_mostrar_apagar').text('Mostrar')
+            $('li').fadeOut(2);
         }else{
+            $('#btn_mostrar_apagar').text('Apagar')
             $('li').fadeIn(1)
+            pinta('white')
         }
     })
 
     //Pintar da cor q deseja a lista
-    function pinta (cor) {
-        $('#pintar_btn').on('click', pintar)
-        function pintar() {
-            $('li').css({'color': cor})
+    $('#pintar_btn').on('click', function() {
+        let corDigitada = $('#corInput').val();
+        pinta(corDigitada);
+        $('#corInput').val('')
+    });
+    function pinta(cor) {
+        $('li').css({'color': cor});
+        if (!cor || !isValidColor(cor)){
+            $('#mensagem_erro').text('Digite uma cor valida').fadeIn(500).fadeOut(3000)
+        }else {
+            $('li').css({'color': cor});
+            $('#mensagem_erro').text(''); 
         }
     }
-    pinta('blue')
+
+    //valida se a cor existe
+    function isValidColor(cor){
+        let coresValidas = ['blue', 'green', 'red', 'black', 'white', 'pink']
+        return coresValidas.includes(cor.toLowerCase())
+    }
+
+    
 
 })
